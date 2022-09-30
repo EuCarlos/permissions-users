6dbda49584ad9b7fdef9e24b2e3b6d967a7a62f8
Carlos Alve <carlosalves369m@hotmail.com>
2022-09-30 15:08:32 -0300

    :hammer: update `routes/index` and ormconfig.json

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,13 +1,9 @@
 const express = require('express')
 const router = express.Router()
-var bodyParser = require('body-parser')
 
 import { JsonResponse } from '../../concerns/response'
 
 router
-    .use(bodyParser.urlencoded({ extended: false }))
-    .use(bodyParser.json())
-
     .get('/', (req, res) => {
         var result = new JsonResponse("Request Successfully", true, {
             name: "Carlos Alves",
@@ -16,11 +12,6 @@ router
         }).response()
 
         return res.status(200).json(result)
-    })
-
-    .post('/', (req, res) => {
-        var result = new JsonResponse("Request Successfully", true, req.body)
-        res.status(201).json(result)
     })
 
 module.exports = router
