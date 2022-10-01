58083f16b5c9f58285a6bc535fd253e29a735ac3
Carlos Alve <carlosalves369m@hotmail.com>
2022-10-01 09:05:50 -0300

    :fire: remove old test

--- src/tests/FirstTest.spec.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../models/User'
-
-test('it should be ok', () => {
-  const user = new User()
-
-  user.name = 'Carlos alves'
-
-  expect(user.name).toEqual('Carlos alves')
-})
